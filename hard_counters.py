14f603f630a4631cdcf444694331929d82d32ff9
Kevin Gilbert <kgilbert.cmu@gmail.com>
2014-03-05 14:24:30 -0500

    refactored hard_counters.py to be run from outside of Python environment

--- hard_counters.py
+++ hard_counters.py
@@ -1,25 +1,15 @@
-def weaknesses(primary, secondary = None):
-	p = chart[primary]
-	if secondary == None:
-		return [k for (k,v) in p.items() if v > 1]
-	s = chart[secondary]
-	weak = []
-	for t in types:
-		if p.hitBy(t) * s.hitBy(t) > 1:
-			weak.append(t)
-	return weak
-	
+import Type
+
 hardCounters = []
-# hard counters
-for t1 in types: 
-	for t2 in types:
-		if chart[t2].hitBy(t1) > 1 and chart[t1].hitBy(t2) < 1:
+for t1 in Type.types: 
+	for t2 in Type.types:
+		if Type.chart[t2].hitBy(t1) > 1 and Type.chart[t1].hitBy(t2) < 1:
 			# print t1, "\thard counters", t2
 			hardCounters.append((t1,t2))
-	if chart[t1].hitBy(t1) == 0.5:
+	if Type.chart[t1].hitBy(t1) == 0.5:
 		# print t1, "resists itself"
 		pass
-	if chart[t1].hitBy(t1) == 2:
+	if Type.chart[t1].hitBy(t1) == 2:
 		# print t1, "kills itself"
 		pass
 		
@@ -30,7 +20,7 @@ for (t1,t2) in hardCounters:
 	else:
 		HC[t1].append(t2)
 		
-HCTypes = [t for t in types if t in HC.keys() and any([t in Ts for Ts in HC.values()])]
+HCTypes = [t for t in Type.types if t in HC.keys() and any([t in Ts for Ts in HC.values()])]
 
 triangles = set()
 
@@ -45,3 +35,6 @@ for t1 in HCTypes:
 				continue
 			else:
 				triangles.add(frozenset([t1, t2, t3]))
+
+for tri in triangles:
+	print tri
