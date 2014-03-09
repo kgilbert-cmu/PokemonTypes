647903b9c897755efd68e1ffc730675da87c46ae
Kevin Gilbert <kgilbert.cmu@gmail.com>
2014-03-09 16:35:41 -0400

    hard_counters.py now supports 'weak' counters

--- hard_counters.py
+++ hard_counters.py
@@ -1,7 +1,7 @@
 import Type
 from optparse import OptionParser
 
-hardCounters = []
+Counters = []
 
 def main():
 	usage = "usage: %prog [options] arg"
@@ -11,16 +11,16 @@ def main():
 
 	(options, args) = parser.parse_args() # user input is stored in "options"
 	
-	global hardCounters
+	global Counters
 	
 	for t1 in Type.types: 
 		for t2 in Type.types:
-			if options.weak and Type.chart[t2].hitBy(t1):
+			if options.weak and Type.chart[t2].hitBy(t1) > 1:
 				# print t1, "\tkills", t2
-				hardCounters.append((t1,t2))
+				Counters.append((t1,t2))
 			elif Type.chart[t2].hitBy(t1) > 1 and Type.chart[t1].hitBy(t2) < 1:
 				# print t1, "\thard counters", t2
-				hardCounters.append((t1,t2))
+				Counters.append((t1,t2))
 		if Type.chart[t1].hitBy(t1) == 0.5:
 			# print t1, "resists itself"
 			pass
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     main()
 			
 HC = {}
-for (t1,t2) in hardCounters:
+for (t1,t2) in Counters:
 	if t1 not in HC.keys():
 		HC[t1] = [t2]
 	else:
@@ -52,7 +52,10 @@ for t1 in HCTypes:
 			elif not (t1 in HC[t2] or t1 in HC[t3]):
 				continue
 			else:
-				triangles.add(frozenset([t1, t2, t3]))
+				fs = frozenset([t1, t2, t3])
+				if len(fs) < 3:
+					continue
+				triangles.add(fs)
 
 for tri in triangles:
 	print tri
